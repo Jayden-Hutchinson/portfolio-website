eda3ffa8892f06dadfd94955611f06f17c8c4bfe
Jayden H <jaydenhutchinson747@gmail.com>
2026-08-14 18:57:03 -0700

    updated location

--- page/src/components/Footer.tsx
+++ page/src/components/Footer.tsx
@@ -1,4 +1,4 @@
-const LOCATION = "Coquitlam, BC, Canada";
+const LOCATION = "Port Coquitlam, BC, Canada";
 const EMAIL = "smokd@gmail.com";
 // const ESTABLISHED = "2024";
 const COMPANY_NAME = "SMOKD";
