f6bd493e1683f897cfd742542e35f3c4fec77f3e
Jayden H <jaydenhutchinson747@gmail.com>
2026-08-26 22:25:23 -0700

    updated email to be business email

--- worker/src/createOderEmail.tsx
+++ worker/src/createOderEmail.tsx
@@ -2,14 +2,12 @@ import { Email, OrderInformation } from './types';
 import { OrderEmail } from '../emails/OrderEmail';
 import { businessInfo } from './businessInfo';
 
-const to = 'jaydenh747@gmail.com';
-
 export function createOrderEmail(orderInformation: OrderInformation): Email {
 	const fullName = `${orderInformation.firstName} ${orderInformation.lastName}` || 'New Order';
 
 	return {
 		from: businessInfo.domain,
-		to: to,
+		to: businessInfo.email,
 		subject: `${fullName}`,
 		react: <OrderEmail {...orderInformation} />,
 	};
