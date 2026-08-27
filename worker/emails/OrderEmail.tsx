109e3e8d48a901fe552c887d75850205cf6556db
Jayden H <jaydenhutchinson747@gmail.com>
2026-08-26 23:08:36 -0700

    updated email format

--- worker/emails/OrderEmail.tsx
+++ worker/emails/OrderEmail.tsx
@@ -4,23 +4,38 @@ import { Tailwind } from 'react-email';
 
 export function OrderEmail({ firstName, lastName, email, streetAddress, city, province, postalCode, products }: OrderInformation) {
 	const fullName = `${firstName} ${lastName}`;
+	const totalPrice = products.reduce((total, product) => total + product.price * product.quantity, 0);
 
 	return (
 		<Html>
 			<Head />
 			<Tailwind>
 				<Body>
-					<strong>{fullName}</strong>
-					<Text>{email}</Text>
-					<Text>
-						{streetAddress}, {city}, {province}, {postalCode}
-					</Text>
-					<table className="text-center w-[50%]">
+					<table className="w-[50%] mb-10">
+						<tbody>
+							<tr>
+								<td>Name:</td>
+								<td>{fullName}</td>
+							</tr>
+							<tr>
+								<td>Email:</td>
+								<td>{email}</td>
+							</tr>
+							<tr>
+								<td>Address:</td>
+								<td>
+									{streetAddress}, {city}, {province}, {postalCode}
+								</td>
+							</tr>
+						</tbody>
+					</table>
+
+					<table className="w-[50%]">
 						<thead>
 							<tr>
-								<th>Product</th>
-								<th>Quantity</th>
-								<th>Total</th>
+								<th className="text-left pb-2">Product</th>
+								<th className="text-left pb-2">Qty.</th>
+								<th className="text-left pb-2">Total</th>
 							</tr>
 						</thead>
 
@@ -33,6 +48,11 @@ export function OrderEmail({ firstName, lastName, email, streetAddress, city, pr
 										<td>${(product.price * product.quantity).toFixed(2)}</td>
 									</tr>
 								))}
+							<tr>
+								<td className="py-5"></td>
+								<td className="font-bold">Total</td>
+								<td>${totalPrice.toFixed(2)}</td>
+							</tr>
 						</tbody>
 					</table>
 				</Body>
