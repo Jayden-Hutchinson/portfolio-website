24e37771e1fcce3fa542cb2a75724e68d10b32ca
Jayden H <jaydenhutchinson747@gmail.com>
2026-09-14 14:51:35 -0700

    removed debug border

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,7 +3,7 @@ import BounceText from "./BounceText";
 
 function Home() {
   return (
-    <div className="flex h-150 w-280 items-center justify-between border border-white/10 text-white">
+    <div className="flex h-150 w-280 items-center justify-between text-white">
       <div className="animate-fade-in-right flex flex-col justify-center gap-4">
         <div className="text-8xl font-bold">Jayden Hutchinson</div>
         <div className="font-mono text-3xl text-neutral-300">
